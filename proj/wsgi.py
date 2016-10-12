bdd4f3def4e3f5bdf58f6e2e2e93c56a4fe11ba7
Nanjiang Shu <nanjiang.shu@bils.se>
2016-10-12 10:33:54 +0200

    add /usr/local/lib explicitly to LD_LIBRARY_PATH in wsgi.py

--- proj/wsgi.py
+++ proj/wsgi.py
@@ -33,6 +33,11 @@ sys.path.append("/usr/local/lib/python2.7/dist-packages")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj.settings")
 
+if 'LD_LIBRARY_PATH' in os.environ:
+    os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH'] + ":" + "/usr/local/lib"
+else:
+    os.environ['LD_LIBRARY_PATH'] = "/usr/local/lib"
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
