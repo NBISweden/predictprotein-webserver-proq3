14e0f6f0a37c35e2e45e76e5a6e1c210fdc5236e
nanjiangshu <nanjiang.shu@gmail.com>
2018-05-04 10:29:56 +0200

    add date_str

--- proj/pred/app/qd_fe.py
+++ proj/pred/app/qd_fe.py
@@ -904,6 +904,7 @@ def GetResult(jobid, query_para):#{{{
                                 msg = "copyfile %s.zip -> %s"%(md5_key, zipfile_profilecache)
                                 myfunc.WriteFile("[%s] %s\n"%(date_str, msg), gen_logfile, "a", True)
                             except Exception as e:
+                                date_str = time.strftime("%Y-%m-%d %H:%M:%S")
                                 msg = "copyfile %s.zip -> %s failed with message %s"%(
                                         md5_key, zipfile_profilecache, str(e))
                                 myfunc.WriteFile("[%s] %s\n"%(date_str, msg), gen_errfile, "a", True)
