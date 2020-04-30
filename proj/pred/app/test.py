00eacded9a221934274f7b23177c2419f59c07a1
nanjiangshu <nanjiang.shu@gmail.com>
2020-04-30 16:00:33 +0200

    update test.py

--- proj/pred/app/test.py
+++ proj/pred/app/test.py
@@ -7,6 +7,18 @@ import webserver_common
 rundir = os.path.dirname(__file__)
 basedir = os.path.realpath("%s/../"%(rundir))
 
+progname=os.path.basename(sys.argv[0])
+
+general_usage = """
+usage: %s TESTMODE options
+"""%(sys.argv[0])
+
+numArgv = len(sys.argv)
+if numArgv <= 1:
+    print( general_usage)
+    sys.exit(1)
+TESTMODE=sys.argv[1]
+
 if 0:#{{{
     infile = sys.argv[1]
     li = myfunc.ReadIDList2(infile, 2, None)
@@ -23,23 +35,23 @@ if 0:#{{{
    print seqRecordList
 #}}}
 
-if 0:#{{{
-    size = float(sys.argv[1])
+if TESTMODE == "size_byte2human": #{{{
+    size = float(sys.argv[2])
     print "size=",size
     print "humansize=", myfunc.Size_byte2human(size)#}}}
 
-if 0:# {{{
+if TESTMODE == "readnews":# {{{
     newsfile = "%s/static/doc/news.txt"%(basedir)
     newsList = myfunc.ReadNews(newsfile)
     print newsList
 # }}}
 
-if 0:
+if TESTMODE == 'readruntime':# {{{
     timefile = "/home/nanjiang/tmp/time.txt"
     runtime = webserver_common.GetRunTimeFromTimeFile(timefile, keyword="model_0")
     print runtime
-
-if 1:
-    pdbfile = "/media/data3/server/web_common_backend/proj/pred/static/result/rst_lovfGb/query.pdb"
+# }}}
+if TESTMODE == 'pdb2seq':
+    pdbfile = sys.argv[2]
     seq = myfunc.PDB2Seq(pdbfile)[0]
     print seq
